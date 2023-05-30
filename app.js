dc5ae05eed34e010307bf3435e02fe3e00b3828f
Santiago Pagge <santy.pagge@gmail.com>
2023-05-30 20:06:02 -0300

    some changes

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ function initCanvas(){
     let enemiesPic1 = new Image();
     let enemiesPic2 = new Image();
 
-    backgroundImage.src = '../JuegoNaves/images/background-pic.jpg'
-    naveImage.src = '../JuegoNaves/images/spaceship-pic.png'
-    enemiesPic1.src = '../JuegoNaves/images/enemigo1.png'
-    enemiesPic2.src = '../JuegoNaves/images/enemigo2.png'
+    backgroundImage.src = 'images/background-pic.jpg'
+    naveImage.src = 'images/spaceship-pic.png'
+    enemiesPic1.src = 'images/enemigo1.png'
+    enemiesPic2.src = 'images/enemigo2.png'
 
     let canvasWidth = ctx.canvas.width;
     let canvasHeight = ctx.canvas.height;
 
-    const enemyTemplate = (options) => {
+    function enemyTemplate(options){
         return {
             id: options.id || '',
             x: options.x || '',
             y: options.y || '',
             width: options.width || '',
-            heigth: options.heigth || '',
+            height: options.height || '',
             image: options.image || enemiesPic1,
         }
     }
@@ -28,12 +28,19 @@ function initCanvas(){
         new enemyTemplate({id: 'enemy3', x: 350, y: 50, width: 80, height: 30})
     ];
 
-    const renderEnemies = (enemyList) => {
+    function renderEnemies(enemyList){
         for(let i = 0; i < enemyList.length; i++){
             let enemy = enemyList[i]
-            ctx.drawImage(enemy.image, enemy.x, enemy.y, enemy.width, enemy.heigth)
+            ctx.drawImage(enemy.image, enemy.x, enemy.y, enemy.width, enemy.height)
         }
     }
+
+    function animate(){
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+        renderEnemies(enemies);
+    }
+
+    let animateInterval = setInterval(animate, 6)
 
 };
 
